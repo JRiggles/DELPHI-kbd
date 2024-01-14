0fde7b8ae8eeaa327159fb9b455b16dae0b67f62
John Riggles <jriggles@icloud.com>
2024-01-14 15:52:15 -0500

    Update boot.py
    Remove bootcfg stuff since it wasn't working anyway

--- firmware/boot.py
+++ firmware/boot.py
@@ -1,12 +1,1 @@
-from kmk.bootcfg import bootcfg
-
-try:
-    bootcfg(
-        midi=False,
-        mouse=False,
-        usb_id=('JRiggles', 'D.E.L.P.H.I Kbd')
-    )
-except Exception as err:
-    print(f'D.E.L.P.H.I boot error: {err}')
-else:
-    print('BOOT OK')
+print('BOOT OK')
